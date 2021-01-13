7bda6bc4d4b4a2832bbb56632c6871d799ed570b
Abdulsalam B <52729566+OfPotsAndPans@users.noreply.github.com>
2021-01-13 15:12:34 -0500

    Add files via upload

---
+++ SPIFFS Practice Program/sketch_jan05c/sketch_jan05c.ino
@@ -0,0 +1,98 @@
+#include <ESP8266WiFi.h>
+#include <WiFiClient.h>
+#include <ESP8266WiFiMulti.h>
+#include <ESP8266mDNS.h>
+#include <ESP8266WebServer.h>
+#include <FS.h>   // Include the SPIFFS library
+
+/*
+ * Abdulsalam Bdeir
+ * 1/2/21 - 1/5/21
+ * This sketch has been altered and changed repeatedly, the orignal version has been overwriten
+ * This project was done to learn how to store and compress data directly in the ESP8266 using it's SPIFFS 
+ * Orignally I thought I could record audio directly as .wav files and compress them in the ESP8266 to then transfer them 
+ * from the ESP8266 to an HTML page where I could have a python program that then downloads the wave file for later. 
+ * Project Source: https://tttapa.github.io/ESP8266/Chap01%20-%20ESP8266.html 
+ */
+
+
+ESP8266WiFiMulti wifiMulti;     // Create an instance of the ESP8266WiFiMulti class, called 'wifiMulti'
+ESP8266WebServer server(80);    // Create a webserver object that listens for HTTP request on port 80
+
+String getContentType(String filename); // convert the file extension to the MIME type
+bool handleFileRead(String path);       // send the right file to the client (if it exists)
+
+
+const char* ssid     = "Jharnka";         // The SSID (name) of the Wi-Fi network you want to connect to
+const char* password = "removed";     // The password of the Wi-Fi network -- has been removed for security reasons
+
+
+void setup() {
+  Serial.begin(115200);         // Start the Serial communication to send messages to the computer
+  delay(10);
+  Serial.println('\n');
+
+  wifiMulti.addAP(ssid, password);   // add Wi-Fi networks you want to connect to
+
+  Serial.println("Connecting ...");
+  int i = 0;
+  while (wifiMulti.run() != WL_CONNECTED) { // Wait for the Wi-Fi to connect
+    delay(250);
+    Serial.print('.');
+  }
+  Serial.println('\n');
+  Serial.print("Connected to ");
+  Serial.println(WiFi.SSID());              // Tell us what network we're connected to
+  Serial.print("IP address:\t");
+  Serial.println(WiFi.localIP());           // Send the IP address of the ESP8266 to the computer
+
+  if (MDNS.begin("esp8266")) {              // Start the mDNS responder for esp8266.local
+    Serial.println("mDNS responder started");
+  } else {
+    Serial.println("Error setting up MDNS responder!");
+  }
+
+  SPIFFS.begin();                           // Start the SPI Flash Files System
+  
+  server.onNotFound([]() {                              // If the client requests any URI
+    if (!handleFileRead(server.uri()))                  // send it if it exists
+      server.send(404, "text/plain", "404: Not Found"); // otherwise, respond with a 404 (Not Found) error
+  });
+
+  server.begin();                           // Actually start the server
+  Serial.println("HTTP server started");
+}
+
+void loop(void) {
+  server.handleClient();
+}
+
+String getContentType(String filename) { // convert the file extension to the MIME type
+  if (filename.endsWith(".html")) return "text/html";
+  else if (filename.endsWith(".css")) return "text/css";
+  else if (filename.endsWith(".js")) return "application/javascript";
+  else if (filename.endsWith(".ico")) return "image/x-icon";
+  /*
+   * Modern web browsers accept compressed files as a response so take advantage of this by adding the GNU zip file type and 
+   * uploading compressed versions of the html and icon files to the SPIFFS, in order to save space and bandwidth.
+  */
+  return "text/plain";
+}
+
+bool handleFileRead(String path) { // send the right file to the client (if it exists)
+  Serial.println("handleFileRead: " + path);
+  if (path.endsWith("/")) path += "index.html";         // If a folder is requested, send the index file
+  String contentType = getContentType(path);            // Get the MIME type
+  String pathWithGz = path + ".gz";
+  if(SPIFFS.exists(pathWithGz) || SPIFFS.exists(path)){  // If the file exists, either as a compressed archive, or normal
+    if(SPIFFS.exists(pathWithGz))                          // If there's a compressed version available
+      path += ".gz";                                         // Use the compressed version
+    File file = SPIFFS.open(path, "r");                    // Open the file
+    size_t sent = server.streamFile(file, contentType);    // Send it to the client
+    file.close();                                          // Close the file again
+    Serial.println(String("\tSent file: ") + path);
+    return true;
+  }
+  Serial.println(String("\tFile Not Found: ") + path);
+  return false;                                          // If the file doesn't exist, return false
+}
